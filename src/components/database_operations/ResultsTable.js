af1468a7a4953f30a149eded90691a1a05f8a6bc
JoeClarkMcGhee <joeclarkmcghee@yahoo.co.uk>
2021-07-29 20:25:19 +0100

    Render error messages for bad quires

--- src/components/database_operations/ResultsTable.js
+++ src/components/database_operations/ResultsTable.js
@@ -8,26 +8,30 @@ class ResultsTable extends Component {
     }
 
     getHeader = function () {
-        let keys = Object.keys(this.props.data[0]);
-        return keys.map((key) => {
-            return (
-                <th>
-                    {key.toUpperCase()}
-                </th>
-            );
-        })
+        if (!this.props.data.includes("bad query")) {
+            let keys = Object.keys(this.props.data[0]);
+            return keys.map((key) => {
+                return (
+                    <th>
+                        {key.toUpperCase()}
+                    </th>
+                );
+            })
+        }
     }
 
     getRows = function () {
         let items = this.props.data;
-        let keys = Object.keys(this.props.data[0]);
-        return items.map((row) => {
-            return (
-                <tr>
-                    <Row data={row} keys={keys}/>
-                </tr>
-            );
-        })
+        if (!items.includes("bad query")) {
+            let keys = Object.keys(this.props.data[0]);
+            return items.map((row) => {
+                return (
+                    <tr>
+                        <Row data={row} keys={keys}/>
+                    </tr>
+                );
+            })
+        }
     }
 
     render() {
@@ -38,9 +42,10 @@ class ResultsTable extends Component {
                         {this.getHeader()}
                     </tr>
                     <tbody>
-                        {this.getRows()}
+                    {this.getRows()}
                     </tbody>
                 </table>
+                {this.props.data.includes("bad query") ? <h2>{this.props.data}</h2> : null}
             </div>
         );
     }
